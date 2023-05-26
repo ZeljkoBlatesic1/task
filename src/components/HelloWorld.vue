b8ac2958d127c86b99c54e73b6ef51d3477b87e1
ZeljkoBlatesic1 <popalion901@gmail.com>
2023-05-26 20:37:34 +0200

    Fix image showing
    Image file where not showing on with v-img.
    Fixing is done with these steps:
    
    1 - in the vue file, at top of the script, import image:
        import logo from '../assets/logo3.png';
    
    2 - in the data created property that allow acces to the logo:
        logoImage: logo
    
    3 - used the property 'logoImage' inside template:
        <v-img :src="logoImage" class="my-3"

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
+          :src="logoImage"
           class="my-3"
           contain
           height="200"
@@ -92,6 +93,7 @@
 </template>
 
 <script>
+import logo from '../assets/logo.svg';
 
 export default {
   name: 'HelloWorld',
@@ -143,6 +145,7 @@ export default {
         href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
       },
     ],
+    logoImage: logo,
   }),
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router';
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 loadFonts()
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 createApp(App)
   .use(vuetify)
