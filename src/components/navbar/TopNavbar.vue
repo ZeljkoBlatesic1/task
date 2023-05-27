89ea1ed9920f7d2d56f158ad64a8575eb461082f
ZeljkoBlatesic1 <popalion901@gmail.com>
2023-05-27 18:34:16 +0200

    Create left, top navbar, footer
    Placeholders for left and top navbars
    are created, as well as placeholders
    for dashboard and footer. Base style
    is done by using vuetify.

---
+++ src/components/navbar/TopNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+      <v-toolbar-title>Top navbar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Link 1</v-btn>
+      <v-btn text>Link 2</v-btn>
+</template>
+  
+
+<script>
+export default {
+  name: 'TopNavbar',
+
+  data () {
+      return {
+      }
+  },
+}
+</script>
